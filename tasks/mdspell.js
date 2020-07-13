817304e17ab6133c9b1c98791a4ae2dab5f1130b
redaxmedia <info@redaxmedia.com>
2020-07-13 22:24:49 +0200

    Join the concept and introduction page

--- tasks/mdspell.js
+++ tasks/mdspell.js
@@ -9,9 +9,9 @@ module.exports = () =>
 				src:
 				[
 					'documentation/**/*.md',
-					'!documentation/02.classes/03.structural-classes.md',
-					'!documentation/02.classes/05.type-classes.md',
-					'!documentation/02.classes/07.functional-classes.md'
+					'!documentation/01.classes/03.structural-classes.md',
+					'!documentation/01.classes/05.type-classes.md',
+					'!documentation/01.classes/07.functional-classes.md'
 				]
 			},
 			options:
