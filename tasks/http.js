515a403f1b8f726541742d0c59504d26a1e9ee40
Henry Ruhs <info@redaxmedia.com>
2020-11-30 18:51:13 +0100

    Update http.js

--- tasks/http.js
+++ tasks/http.js
@@ -12,7 +12,7 @@ module.exports = () =>
 				{
 					'Accept': 'application/vnd.github.v3+json',
 					'Authorization': 'token ' + process.env.TRIGGER_TOKEN,
-					'User-Agent': 'ncss-documentation'
+					'User-Agent': 'trigger'
 				},
 				body: JSON.stringify(
 				{
